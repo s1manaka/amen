f81d78004489552e1d00d67cb39129ac4032b4ea
s1manaka <kinakomoti0421@gmail.com>
2024-12-23 18:08:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -311,8 +311,8 @@ function endGame() {
 }
 
 function adjustCanvasSize() {
-    const width = window.innerWidth * 0.9; // キャンバスの幅を少し小さく
-    const height = window.innerHeight * 0.9; // キャンバスの高さを少し小さく
+    const width = window.innerWidth * 0.8; // キャンバスの幅をさらに小さく
+    const height = window.innerHeight * 0.8; // キャンバスの高さをさらに小さく
     canvas.width = width;
     canvas.height = height;
     canvas.style.width = `${width}px`;
